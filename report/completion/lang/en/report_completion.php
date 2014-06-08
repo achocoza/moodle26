298a0c694c9e03b11b4bf662d6162afa681296dc
Rajneel Totaram <totaram_r@usp.ac.fj>
2014-06-09 11:18:20 +1200

    MDL-39156 completion report: Excel complains when CSV file starts with characters "ID"
    Not really a Moodle bug but changing string "ID" to "User ID" avoids this problem.

--- report/completion/lang/en/report_completion.php
+++ report/completion/lang/en/report_completion.php
@@ -26,7 +26,7 @@
 
 $string['completion:view'] = 'View course completion report';
 $string['completiondate'] = 'Completion date';
-$string['id'] = 'ID';
+$string['id'] = 'User ID';// RT - "ID" causes Excel to treat this file as SYLK file. See MDL-39156
 $string['name'] = 'Name';
 $string['page-report-completion-x'] = 'Any completion report';
 $string['page-report-completion-index'] = 'Course completion report';
